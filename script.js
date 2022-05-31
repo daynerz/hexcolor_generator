c3da8b4f46cbc7b74bf72cf81cd115a91270c856
Dana Zheng <zhengdana1270@gmail.com>
2022-05-31 00:08:38 -0400

    function myColor

---
+++ script.js
@@ -0,0 +1,13 @@
+function myColor() {
+  
+    // Get value of the selected color
+    var color = document.getElementById('selected_color').value;
+
+    // Set color as background
+    document.body.style.backgroundColor = color;
+
+    // Store hex code
+    document.getElementById('box').value = color;
+}
+
+document.getElementById('selected_color').addEventListener('input', myColor);
